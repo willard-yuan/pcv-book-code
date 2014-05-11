30ce0dd9a6919bf24e1f1d0b4e48a97179e106cd
Willard Yuan <yongyuanstu@gmail.com>
2014-05-11 09:40:13 +0800

    update code in ch07

--- ch07/ch07_addImage.py
+++ ch07/ch07_addImage.py
@@ -1,23 +1,30 @@
+# -*- coding: utf-8 -*-
 import pickle
 from PCV.imagesearch import imagesearch
 from PCV.localdescriptors import sift
 from sqlite3 import dbapi2 as sqlite
 from PCV.tools.imtools import get_imlist
 
+#获取图像列表
 imlist = get_imlist('./first500/')
 nbr_images = len(imlist)
+#获取特征列表
 featlist = [imlist[i][:-3]+'sift' for i in range(nbr_images)]
+
 # load vocabulary
+#载入词汇
 with open('./first500/vocabulary.pkl', 'rb') as f:
     voc = pickle.load(f)
-# create indexer
+#创建索引
 indx = imagesearch.Indexer('testImaAdd.db',voc)
 indx.create_tables()
 # go through all images, project features on vocabulary and insert
+#遍历所有的图像，并将它们的特征投影到词汇上
 for i in range(nbr_images)[:500]:
     locs,descr = sift.read_features_from_file(featlist[i])
     indx.add_to_index(imlist[i],descr)
 # commit to database
+#提交到数据库
 indx.db_commit()
 
 con = sqlite.connect('testImaAdd.db')
